739a69377d934dab9bcb898e8e849e10f059503b
Genady <gperchenko@gmail.com>
2018-08-01 21:55:23 -0400

    Add project files.

---
+++ EFTests.DataAccess/TestContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using EFTests.Entities;
+
+namespace EFTests.DataAccess
+{
+    public class TestContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Server =(localdb)\\ProjectsV13; Database=EFCore2Tests; Trusted_Connection=true;",
+                  x => x.MigrationsAssembly("EFTests.Migrations"));
+        }
+
+    }
+}
